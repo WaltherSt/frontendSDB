3293baa74fc30fb11cb309524addb1305a5c574a
Walter Gomez St <129338273+WaltherSt@users.noreply.github.com>
2024-05-04 09:58:34 -0500

    inicialización frontEnd del proyecto SDB

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"amarillo-sazon": "#f3c332",
+
+				"negro-sazon": "#0a0a0a",
+			},
+		},
+	},
+	plugins: [],
+};
