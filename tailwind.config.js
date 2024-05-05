f8a4967cee9ad2d81ef84df755ae7e8e8028ef76
Walter Gomez St <129338273+WaltherSt@users.noreply.github.com>
2024-05-05 15:12:29 -0500

    se agregan logos de la pagina

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,28 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"amarillo-sazon": "#f3c332",
-
-				"negro-sazon": "#0a0a0a",
+				// "amarillo-sazon": "#f3c332",
+				// "amarillo-sazon": "#f3c332",1
+				// -----------------------------------------------------------
+				"amarillo-sazon": {
+					900: "#F2E0C9",
+					800: "#D5C5B1",
+				},
+				// ------------------------------------------------------------
+				// "amarillo-sazon": "#F2CC85", 3
+				// "amarillo-sazon": "#D9CA82",4
+				// "amarillo-sazon": "#D1D9B4",5
+				// "amarillo-sazon": "#A0A603",6
+				// "amarillo-sazon": "#F2F2F2",7
+				// -------------------------------------------------------
+				// "negro-sazon": "#0a0a0a",
+				// "negro-sazon": "#0a0a0a",1
+				"negro-sazon": "#40322A",
+				// 	"negro-sazon": "#594736",3
+				// "negro-sazon": "#172601",4
+				// "negro-sazon": "#2B402C",5
+				// "negro-sazon": "#0D0D0D",6
+				// "negro-sazon": "#593F28",7
 			},
 		},
 	},
