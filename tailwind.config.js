16d5a5cc5d50359363682ae0f56d40afa0cae39b
JhonnyArevaloS <jhonnyarevalo33@gmail.com>
2024-05-09 19:18:30 -0500

    interfaz vista de receta

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
+
+		borderWidth:{
+			DEFAULT: '1px',
+			'0':'0',
+			'1':'1px',
+			'2':'2px',
+			'3':'3px',
+			'4':'4px',
+			'6':'6px',
+			'8':'8px',
+			'60':'60px',
+		},
+
 		extend: {
 			colors: {
 				"amarillo-sazon": "#f3c332",
